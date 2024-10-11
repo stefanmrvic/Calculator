dc6a44faf806b45cddebc769d989fea667239ed1
Stefan <61718109+stefanmrvic@users.noreply.github.com>
2024-10-11 09:46:06 +0200

    Add: Basic HTML + add, subtract, divide, and multiply JS functions

---
+++ main.js
@@ -0,0 +1,19 @@
+let operator;
+let operandA;
+let operandB;
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
