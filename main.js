4f7ec69d77b087cfb358fffd301b7efe63493082
Stefan <61718109+stefanmrvic@users.noreply.github.com>
2024-12-07 15:11:11 +0100

    Add: Switch statement logic for buttons

--- main.js
+++ main.js
@@ -1,7 +1,3 @@
-let operator;
-let operandA;
-let operandB;
-
 function add(a, b) {
     return a + b;
 }
@@ -21,16 +17,68 @@ function divide(a, b) {
 let buttons = document.querySelectorAll('button');
 let screen = document.querySelector('.screen p');
 
+let operator;
+let operandA;
+let operandB;
+let dotIsPresent = false;
+
 buttons.forEach(button => {
     button.addEventListener('click', (e) => {
         let clickedButton = e.target.textContent.trim();
 
-        if (clickedButton === 'CLEAR') screen.textContent = '0';
-        else if (clickedButton === 'DELETE') { 
-            if (screen.textContent.length === 1) screen.textContent = 0;
-            else screen.textContent = screen.textContent.slice(0, -1);
+        switch(clickedButton) {
+            case 'CLEAR':
+                operandA = '';
+                operandB = '';
+                screen.textContent = '0';
+                break;
+            case 'DELETE':
+                if (screen.textContent.length === 1) screen.textContent = 0;
+                else screen.textContent = screen.textContent.slice(0, -1);
+                break;
+            case '0':
+                if (screen.textContent === '0') {
+                    screen.textContent = e.target.textContent;
+                } else {
+                    screen.textContent += e.target.textContent;
+                }
+                break;
+            case '.':
+                // if (!operandA.includes('.') || !) {
+                //     screen.textContent += e.target.textContent;
+                // }
+                break;
+            case '+':
+                operandA = screen.textContent;
+                operator = '+';
+                screen.textContent = '0';
+                break;
+            case '-':
+                operandA = screen.textContent;
+                operator = '-';
+                screen.textContent = '0';
+                break;
+            case '×':
+                operandA = screen.textContent;
+                operator = '*';
+                screen.textContent = '0';
+                break;
+            case '÷':
+                operandA = screen.textContent;
+                operator = '/';
+                screen.textContent = '0';
+                break;
+            case '=':
+                operandB = screen.textContent.trim();
+                console.log(operandA);
+                console.log(operandB);
+            default:
+                if (screen.textContent === '0') {
+                    screen.textContent = e.target.textContent;
+                } else {
+                    screen.textContent += e.target.textContent;
+                }
+                break;
         }
-        else if (screen.textContent === '0') screen.textContent = e.target.textContent;
-        else screen.textContent += e.target.textContent;
     })
 })
