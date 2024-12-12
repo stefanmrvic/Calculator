b6f136b7d07a4924e16313efc7c441244c4d00fc
Stefan <61718109+stefanmrvic@users.noreply.github.com>
2024-12-12 16:16:45 +0100

    Remove: Old calculator logic

--- main.js
+++ main.js
@@ -10,95 +10,11 @@ buttons.forEach(button => {
   button.addEventListener('click', (e) => {
     let clickedButton = e.target.textContent.trim();
 
-    switch (clickedButton) {
-      case 'CLEAR':
-        operandA = '';
-        operandB = '';
-        operator = '';
-        screen.textContent = '0';
-        break;
-      case 'DELETE':
-        if (screen.textContent.length === 1) screen.textContent = 0;
-        else screen.textContent = screen.textContent.slice(0, -1);
-        break;
-      case '0':
-        if (screen.textContent === '0') {
-          screen.textContent = e.target.textContent;
-        } else {
-          screen.textContent += e.target.textContent;
-        }
-        break;
-      case '.':
-        if (!operandA.includes('.')) {
-             screen.textContent += e.target.textContent;
-         }
-        break;
-      case '+':
-      case '-':
-      case '×':
-      case '÷':
-        operator = clickedButton;
-
-        if (!operandA) {
-          operandA = screen.textContent;
-        } else {
-          operandB = screen.textContent;
-        }
-        
-        screen.textContent = '0';
-        
-        if (operandA && operandB) {
-          let result = operate(operandA, operandB, operator);
-          operandA = result;
-          operandB = '';
-          screen.textContent = result;
-        }
-        break;
-
-      case '=':
-        if (!operandA) {
-          operandA = screen.textContent;
-        } else {
-          operandB = screen.textContent;
-        }
-
-        if (operandA && operandB) {
-          let result = operate(operandA, operandB, operator);
-          operandA = result;
-          operandB = '';
-          screen.textContent = result;
-        }
-        break;
-        
-      default:
-        if (screen.textContent === '0') {
-          screen.textContent = e.target.textContent;
-        } else {
-          screen.textContent += e.target.textContent;
-        }
-        break;
-    }
   })
 })
 
 function operate(operandA, operandB, operator) {
-  let sum;
 
-  if (operandA, operandB) {
-    if (operator === '+') {
-      sum = add(operandA, operandB);
-    }
-    else if (operator === '-') {
-      sum = subtract(operandA, operandB);
-    }
-    else if (operator === '÷') {
-      sum = divide(operandA, operandB)
-    }
-    else if (operator === '×') {
-      sum = multiply(operandA, operandB);
-    }
-  return sum;
-  }
 }
 
 function add(a, b) {
